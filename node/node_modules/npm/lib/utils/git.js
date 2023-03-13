b3a5c07542564e23f56f8578b0282b455327574b
gongiskhan <goncalo.p.gomes@gmail.com>
2023-03-13 15:21:31 +0000

    dev

--- node/node_modules/npm/lib/utils/git.js
+++ node/node_modules/npm/lib/utils/git.js
@@ -1,17 +1,20 @@
-// handle some git configuration for windows
+'use strict'
+
+const BB = require('bluebird')
+
+const exec = require('child_process').execFile
+const spawn = require('./spawn')
+const npm = require('../npm.js')
+const which = require('which')
+const git = npm.config.get('git')
+const assert = require('assert')
+const log = require('npmlog')
+const noProgressTillDone = require('./no-progress-while-running.js').tillDone
 
 exports.spawn = spawnGit
+exports.exec = BB.promisify(execGit)
 exports.chainableExec = chainableExec
 exports.whichAndExec = whichAndExec
-
-var exec = require('child_process').execFile
-var spawn = require('./spawn')
-var npm = require('../npm.js')
-var which = require('which')
-var git = npm.config.get('git')
-var assert = require('assert')
-var log = require('npmlog')
-var noProgressTillDone = require('./no-progress-while-running.js').tillDone
 
 function prefixGitArgs () {
   return process.platform === 'win32' ? ['-c', 'core.longpaths=true'] : []
@@ -19,12 +22,22 @@ function prefixGitArgs () {
 
 function execGit (args, options, cb) {
   log.info('git', args)
-  var fullArgs = prefixGitArgs().concat(args || [])
+  const fullArgs = prefixGitArgs().concat(args || [])
   return exec(git, fullArgs, options, noProgressTillDone(cb))
 }
 
 function spawnGit (args, options) {
   log.info('git', args)
+  // If we're already in a git command (eg, running test as an exec
+  // line in an interactive rebase) then these environment variables
+  // will force git to operate on the current project, instead of
+  // checking out/fetching/etc. whatever the user actually intends.
+  options.env = options.env || Object.keys(process.env)
+    .filter(k => !/^GIT/.test(k))
+    .reduce((set, k) => {
+      set[k] = process.env[k]
+      return set
+    }, {})
   return spawn(git, prefixGitArgs().concat(args || []), options)
 }
 
