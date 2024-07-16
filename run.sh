3893d8c7af8fbf644cc0b308de59d8a6e9cf22df
gongiskhan <goncalo.p.gomes@gmail.com>
2024-07-16 19:47:05 +0100

    dev

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-forever stopall
-killall forever
-killall node
-forever print.js &
+#forever stopall
+#killall forever
+#killall node
+pm2 start print.js
+#forever print.js &
 lt --port 3222 --subdomain tastic-print &
 #forever notify.js --shop=$1 &
 #forever uber.js --shop=$1
