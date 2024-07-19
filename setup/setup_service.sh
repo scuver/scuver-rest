b405acc6a55458f4e1a78e2168a4692110ad0f6e
gongiskhan <goncalo.p.gomes@gmail.com>
2024-07-19 12:45:06 +0100

    deploy

--- setup/setup_service.sh
+++ setup/setup_service.sh
@@ -6,20 +6,20 @@ PASS=tmp12345
 
 sed "s/TARGET_PORT/$TARGET_SSH_PORT/g; s/LT_HOST/$LT_HOST/g" "start_tastic_example.sh" > "start_tastic_GENERATED.sh"
 
-sshpass -p $PASS scp -P $TARGET_SSH_PORT  ~/.ssh/id_rsa $TARGET_IP:/home/ggomes/.ssh/
-sshpass -p $PASS scp -P $TARGET_SSH_PORT  ~/.ssh/id_rsa.pub $TARGET_IP:/home/ggomes/.ssh/
-sshpass -p $PASS scp -P $TARGET_SSH_PORT start_tastic_GENERATED.sh $TARGET_IP:/home/ggomes/start_tastic.sh
-sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo mv /home/ggomes/start_tastic.sh /usr/local/bin/start_tastic.sh && sudo chmod +x /usr/local/bin/start_tastic.sh'
-sshpass -p $PASS scp -P $TARGET_SSH_PORT tastic.service $TARGET_IP:/home/ggomes/tastic.service
-sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo mv /home/ggomes/tastic.service /etc/systemd/system/tastic.service'
-sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl stop tastic'
-sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl disable tastic'
-sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl daemon-reload'
-sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl enable tastic'
-sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl start tastic'
-sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl status tastic'
+/opt/homebrew/bin/sshpass -p $PASS scp -P $TARGET_SSH_PORT  ~/.ssh/id_rsa $TARGET_IP:/home/ggomes/.ssh/
+/opt/homebrew/bin/sshpass -p $PASS scp -P $TARGET_SSH_PORT  ~/.ssh/id_rsa.pub $TARGET_IP:/home/ggomes/.ssh/
+/opt/homebrew/bin/sshpass -p $PASS scp -P $TARGET_SSH_PORT start_tastic_GENERATED.sh $TARGET_IP:/home/ggomes/start_tastic.sh
+/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo mv /home/ggomes/start_tastic.sh /usr/local/bin/start_tastic.sh && sudo chmod +x /usr/local/bin/start_tastic.sh'
+/opt/homebrew/bin/sshpass -p $PASS scp -P $TARGET_SSH_PORT tastic.service $TARGET_IP:/home/ggomes/tastic.service
+/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo mv /home/ggomes/tastic.service /etc/systemd/system/tastic.service'
+/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl stop tastic'
+/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl disable tastic'
+/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl daemon-reload'
+/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl enable tastic'
+/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl start tastic'
+/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl status tastic'
 sleep 20
-#sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo journalctl -u tastic.service -b'
-sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'tail -n50 /home/ggomes/start_tastic.log'
-#sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'tail -n50 /home/ggomes/.pm2/logs/tastic-print-out.log'
-#sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'tail -n50 /home/ggomes/.pm2/logs/tastic-print-error.log'
+#/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo journalctl -u tastic.service -b'
+/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'tail -n50 /home/ggomes/start_tastic.log'
+#/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'tail -n50 /home/ggomes/.pm2/logs/tastic-print-out.log'
+#/opt/homebrew/bin/sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'tail -n50 /home/ggomes/.pm2/logs/tastic-print-error.log'
