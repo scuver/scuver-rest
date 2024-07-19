292002f32a28709a05ce2dd3f1c3dd47417a3836
gongiskhan <goncalo.p.gomes@gmail.com>
2024-07-19 10:49:56 +0100

    deploy

--- setup/setup_service.sh
+++ setup/setup_service.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -ex
 TARGET_SSH_PORT=${1:-2223}
 TARGET_IP=${2:-localhost}
-LT_HOST=${3:varunca-print}
+LT_HOST=${3:-varunca-print}
 PASS=tmp12345
+
+git add .
+git commit -m "deploy"
+git push
+ssh 168.119.202.164  "cd /Users/ggomes/dev/scuver-rest && git pull"
 
 sed "s/TARGET_PORT/$TARGET_SSH_PORT/g; s/LT_HOST/$LT_HOST/g" "start_tastic_example.sh" > "start_tastic_GENERATED.sh"
 
@@ -20,4 +25,6 @@ sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl start tastic
 sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo systemctl status tastic'
 sleep 20
 #sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'sudo journalctl -u tastic.service -b'
-sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'tail -n100 /home/ggomes/start_tastic.log'
+sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'tail -n50 /home/ggomes/start_tastic.log'
+#sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'tail -n50 /home/ggomes/.pm2/logs/tastic-print-out.log'
+#sshpass -p $PASS ssh -p $TARGET_SSH_PORT $TARGET_IP 'tail -n50 /home/ggomes/.pm2/logs/tastic-print-error.log'
