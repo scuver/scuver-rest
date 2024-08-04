68bbdcfb431a0cb7f1eaf811610a309c8f2a8816
gongiskhan <goncalo.p.gomes@gmail.com>
2024-08-04 08:28:41 +0100

    dev

--- setup/start_tastic_example.sh
+++ setup/start_tastic_example.sh
@@ -17,7 +17,7 @@ echo "Starting services..." >> $LOGFILE
 
 # Start SSH tunnel with retries for network issues only
 SSH_COMMAND="$AUTOSSH_BIN -M 0 -N -R TARGET_PORT:localhost:22 168.119.202.164 -i $SSH_IDENTITY"
-#SERVEO_COMMAND="$AUTOSSH_BIN -M 0 -N -p 443 -R0:localhost:3000 -L4300:localhost:4300 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 B1Kely8PXDF@a.pinggy.io"
+PINGGY_COMMAND="$AUTOSSH_BIN -M 0 -N -p 443 -R0:localhost:3222 -L4300:localhost:4300 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 B1Kely8PXDF@a.pinggy.io"
 RETRY_COUNT=0
 MAX_RETRIES=5
 
@@ -26,20 +26,20 @@ $PM2_BIN start /home/ggomes/scuver-rest/print.js --name tastic-print -f >> $LOGF
 $PM2_BIN save >> $LOGFILE 2>&1
 echo "Started pm2 service" >> $LOGFILE
 
-#sleep 1
-#until $SERVEO_COMMAND >> $LOGFILE 2>&1 || [ $RETRY_COUNT -eq $MAX_RETRIES ]; do
-#    if grep -q "Network is unreachable" $LOGFILE; then
-#        echo "Retry SERVEO tunnel due to network issue ($RETRY_COUNT/$MAX_RETRIES)" >> $LOGFILE
-#        RETRY_COUNT=$((RETRY_COUNT + 1))
-#        sleep 10
-#    else
-#        echo "SERVEO authentication failed" >> $LOGFILE
-#    fi
-#done
-#
-#if [ $RETRY_COUNT -eq $MAX_RETRIES ]; then
-#    echo "Failed to establish SERVEO tunnel after $MAX_RETRIES attempts." >> $LOGFILE
-#fi
+sleep 1
+until $PINGGY_COMMAND >> $LOGFILE 2>&1 || [ $RETRY_COUNT -eq $MAX_RETRIES ]; do
+    if grep -q "Network is unreachable" $LOGFILE; then
+        echo "Retry PINGGY_COMMAND tunnel due to network issue ($RETRY_COUNT/$MAX_RETRIES)" >> $LOGFILE
+        RETRY_COUNT=$((RETRY_COUNT + 1))
+        sleep 10
+    else
+        echo "PINGGY_COMMAND authentication failed" >> $LOGFILE
+    fi
+done
+
+if [ $RETRY_COUNT -eq $MAX_RETRIES ]; then
+    echo "Failed to establish SERVEO tunnel after $MAX_RETRIES attempts." >> $LOGFILE
+fi
 
 # Start localtunnel
 sleep 1
@@ -64,9 +64,9 @@ fi
 
 echo "Started SSH tunnel" >> $LOGFILE
 
-while true; do
- ssh -p 443 -R0:localhost:3222 -L4300:localhost:4300 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 B1Kely8PXDF@a.pinggy.io           ;
-sleep 10; done
+#while true; do
+# ssh -p 443 -R0:localhost:3222 -L4300:localhost:4300 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 B1Kely8PXDF@a.pinggy.io           ;
+#sleep 10; done
 
 # Keep script running
 tail -f /dev/null >> $LOGFILE 2>&1
