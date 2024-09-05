f745e8f7a0097517f5a187e39192aac36d3d73d6
gongiskhan <goncalo.p.gomes@gmail.com>
2024-09-05 09:08:55 +0100

    dev

--- setup/cert.sh
+++ setup/cert.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 CONFIG="
 [ req ]
-default_bits        = 4096
-default_md          = sha256
-distinguished_name  = req_distinguished_name
-prompt              = no
-x509_extensions         = v3_req
+distinguished_name=req_distinguished_name
+prompt=no
 [ req_distinguished_name ]
-countryName=        YourCountryCode
-stateOrProvinceName=    YourState
-localityName=       YourTown
-organizationName=   YourCompany
-organizationalUnitName= YourUnit
-commonName=     *
-[ v3_req ]
-basicConstraints = CA:FALSE
-keyUsage = digitalSignature, keyEncipherment
-extendedKeyUsage = serverAuth
-subjectAltName = email:whatever@localhost
-
+countryName=PT
+stateOrProvinceName=Oeiras
+localityName=Oeiras
+organizationName=Tastic
+organizationalUnitName=Tastic
+commonName=localhost
 "
 
 #openssl genrsa -out wildcard.local.key 4096
 #openssl req -new -key wildcard.local.key -out wildcard.local.csr
-openssl x509 -req -in wildcard.local.csr -signkey wildcard.local.key -out wildcard.local.crt -days 365
-#openssl req -x509 -new -nodes -key wild.key \
-#            -config <(echo "$CONFIG") -days 365 \
-#            -out wild.crt
+#openssl x509 -req -in wildcard.local.csr -signkey wildcard.local.key -out wildcard.local.crt -days 365
+openssl req -x509 -new -nodes -key wildcard.local.key \
+            -config <(echo "$CONFIG") -days 365 \
+            -out wildcard.local.crt
